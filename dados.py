e6a7b1e17efdc08302f597545ddd1747b45220cb
Wanderson <wanderson.carelli@gmail.com>
2022-06-05 22:29:19 -0300

    Correção do arquivo com funções do banco de dados.

--- dados.py
+++ dados.py
@@ -12,28 +12,3 @@ class BancoDados:
     # Desconectando o banco de dados
     def desconectar(self):
         self.connect.close()
-
-
-# BancoDados().cursor.execute("""CREATE TABLE IF NOT EXISTS vendas (
-#     id INTEGER PRIMARY KEY,
-#     id_cliente INTEGER(5) NOT NULL,
-#     nome_cliente CHAR(40) NOT NULL,
-#     id_motocicleta INTEGER(5) NOT NULL,
-#     marca_motocicleta CHAR(30) NOT NULL,
-#     modelo_motocicleta CHAR(30) NOT NULL,
-#     ano_motocicleta INTEGER(4) NOT NULL,
-#     preco_motocicleta INTEGER(10) NOT NULL)""")
-
-# BancoDados().cursor.execute("""DROP TABLE vendas""")
-
-# BancoDados().cursor.execute("""CREATE TABLE IF NOT EXISTS motocicletas (
-#     id INTEGER PRIMARY KEY,
-#     marca CHAR(30) NOT NULL,
-#     modelo CHAR(30) NOT NULL,
-#     ano INTEGER(4) NOT NULL,
-#     cilindrada INTEGER(5) NOT NULL,
-#     preco INTEGER(10) NOT NULL,
-#     id_dono INTEGER(5) NOT NULL,
-#     nome_dono CHAR(40) NOT NULL)""")
-
-# BancoDados().cursor.execute("""ALTER TABLE clientes DROP COLUMN qtd_motocicletas""")
